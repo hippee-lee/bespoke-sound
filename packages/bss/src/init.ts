920f95734ffdd482ae9b398b5b93dfd6808f62ef
Matthew Hippely <hippely@gmail.com>
2024-12-20 09:24:23 -0800

    feat(mvp): can play clicks and all other sounds based on pointer location and mouse movement
    Signed-off-by: Matthew Hippely <hippely@gmail.com>

--- packages/bss/src/init.ts
+++ packages/bss/src/init.ts
@@ -3,7 +3,10 @@ import { SoundObservable } from './api/event-observer';
 import { FOCUS_EVENTS, MOUSE_EVENTS, KEYBOARD_EVENTS, POINTER_EVENTS } from './api/events';
 import * as Tone from "tone";
 
-const synth = new Tone.Synth().toDestination();
+const polySynth = new Tone.PolySynth().toDestination();
+
+// Turn the volume down by 10 dB
+polySynth.volume.value = -10;
 
 // Singleton for SoundObservable
 const observable = new SoundObservable();
@@ -21,11 +24,40 @@ const proxiedWindow = createProxy(window, {
     });
 });
 
-// Log all observed events from the Observable
+// Define the major and minor scales
+const majorNotes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5'];
+const minorNotes = ['C4', 'D4', 'D#4', 'F4', 'G4', 'G#4', 'A#4', 'C5'];
+
+// Function to map a value from one range to another
+function mapValue(value: number, inMin: number, inMax: number, outMin: number, outMax: number): number {
+    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
+
+let nonClickCounter = 0;
+// Add an observer to the observable
 observable.addObserver((event) => {
-    if(event.type === 'click') {
-        synth.triggerAttackRelease('C4', "8n");
-        console.log(`Event observed by Observable: ${event.type}`, event.event);
+    if (event.type === 'click') {
+        // Play a single note for clicks
+        polySynth.triggerAttackRelease('C6', '8n');
+    } else {
+        const { clientX, clientY } = event.event as MouseEvent;
+
+        // Map the x-axis (width) to the major scale
+        const xIndex = Math.floor(
+            mapValue(clientX, 0, window.innerWidth, 0, majorNotes.length - 1)
+        );
+
+        // Map the y-axis (height) to the minor scale
+        const yIndex = Math.floor(
+            mapValue(clientY, 0, window.innerHeight, 0, minorNotes.length - 1)
+        );
+
+        // Get the corresponding notes
+        const xNote = majorNotes[Math.min(xIndex, majorNotes.length - 1)];
+        const yNote = minorNotes[Math.min(yIndex, minorNotes.length - 1)];
+
+        // Play the notes together as a chord
+        polySynth.triggerAttackRelease([xNote, yNote], '8n');
     }
 });
 
